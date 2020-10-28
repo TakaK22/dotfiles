2e724d469d4f48b7f4a3c1b418b0a302265f7e67
TakaK22 <25346710+TakaK22@users.noreply.github.com>
2020-10-28 22:44:54 +0900

    Add install.sh

---
+++ bin/install.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+# homebrewがinstallされていなかったら、homebrewをinstall
+if [ ! -e /usr/local/bin/brew ]; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+else
+  printf '\033[33m%s\033[m\n' 'homebrew is already installed'
+fi
+
+# ln -s .vimrc ~/.vimrc
